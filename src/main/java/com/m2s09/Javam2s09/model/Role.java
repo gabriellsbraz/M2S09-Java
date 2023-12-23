37dfde9f8c9e25d6157edbd3bda14f6c159f0742
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-12-23 12:20:58 -0300

    [M02S09] Ex 02 - Mini-Projeto API Veiculos - Entidades JPA

---
+++ src/main/java/com/m2s09/Javam2s09/model/Role.java
@@ -0,0 +1,6 @@
+package com.m2s09.Javam2s09.model;
+
+public enum Role {
+    ADMIN,
+    USUARIO;
+}
---
+++ src/main/java/com/m2s09/Javam2s09/model/TipoVeiculo.java
@@ -0,0 +1,8 @@
+package com.m2s09.Javam2s09.model;
+
+public enum TipoVeiculo {
+    AUTOMOVEL,
+    CAMIONETA,
+    ONIBUS,
+    CAMINHAO;
+}
