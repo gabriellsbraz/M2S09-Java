d4c6b39f1b53e5fe7bcb7699f2ec571ddccae7c2
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-12-23 12:12:44 -0300

    [M02S09] Ex 01 - Mini-Projeto API Veiculos - Criação do Projeto

---
+++ src/main/java/com/m2s09/Javam2s09/JavaM2s09Application.java
@@ -0,0 +1,13 @@
+package com.m2s09.Javam2s09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaM2s09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaM2s09Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2s09/Javam2s09/JavaM2s09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2s09.Javam2s09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaM2s09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
